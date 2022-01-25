6b66e67616cab5dc099370611db208cca2f95561
shreyajindal777 <97687104+shreyajindal777@users.noreply.github.com>
2022-01-25 16:34:25 +0530

    Add files via upload

---
+++ assignment 2 outputs corrected.py
@@ -0,0 +1,43 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+= RESTART: C:\Users\SHREYA JINDAL\AppData\Local\Programs\Python\Python310\inputs assignment 2.py
+(a)
+Give the statement:python is a case sensitive language
+python is a case sensitive language
+35
+(b)
+egaugnal evitisnes esac a si nohtyp
+(c)
+a case sensitive
+(d)
+python is object oriented language
+(e)
+10
+(f)
+pythonisacasesensitivelanguage
+Hey,shreya jindal Here!
+My SID is 21105047
+I am from ECE department and my CGPA is 9.900000
+(a)
+0b1000
+(b)
+0b111010
+(c)
+0b110010
+(d)
+0b11100000
+0b101000
+(e)
+0b1110
+0b0
+Give a number:45
+Give a number:76
+Give a number:98
+98 is greatest number
+Give a word:nameless
+Yes
+Give a number:4
+Give a number:5
+Give a number:3
+The triangle is possible
---
+++ inputs assignment 2.py
@@ -0,0 +1,115 @@
+#question 1
+#(a)
+#given- take input as 'Python is a case sensitive language'
+print("(a)")
+input_string=input("Give the statement:")
+print(input_string)
+length=len(input_string)
+print(length)
+print
+
+#(b)
+print("(b)")
+reverse_string=input_string[length::-1]
+print(reverse_string)
+
+#(c)
+print("(c)")
+new_string=input_string[10:26]
+print(new_string)
+
+#(d)
+print("(d)")
+#replace 'a case sensitive' with 'oject oriented'
+replaced_string=input_string[0:10]+'object oriented'+input_string[26:35]
+print(replaced_string)
+
+#(e)
+print("(e)")
+index_a=input_string.find('a')
+print(index_a)
+
+#(f) first check the index of white spaces and then write the new string by not using those index
+print("(f)")
+remove_whitespaces=input_string.replace(" ","")
+print(remove_whitespaces)
+
+
+#q2
+name="shreya jindal"
+SID=21105047
+department_name="ECE"
+CGPA=9.9
+
+print("Hey,%s Here!"%(name))
+print("My SID is %d"%(SID))
+print("I am from %s department and my CGPA is %f"%(department_name,CGPA))
+
+
+#q3
+#a=56 b=10
+a=0b111000
+b=0b1010
+
+#(a)
+print("(a)")
+#&
+print(bin(a&b))
+
+#(b)
+print("(b)")
+#|
+print(bin(a|b))
+
+#(c)
+print("(c)")
+#^
+print(bin(a^b))
+
+#(d)
+print("(d)")
+#a<<2 b<<2 ,left shift 2
+print(bin(a<<2))
+print(bin(b<<2))
+
+#(e)
+print("(e)")
+#a>>2 b>>4 right shift a with 2 bits and b with 4 bits
+print(bin(a>>2))
+print(bin(b>>4))
+
+
+#q4
+num_1=int(input("Give a number:"))
+num_2=int(input("Give a number:"))
+num_3=int(input("Give a number:"))
+
+if(num_1>num_2):
+    if(num_1>num_3):
+        print(num_1,"is greatest number")
+    else:
+        print(num_3,"is greatest number")
+else:
+    if(num_2>num_3):
+        print(num_2,"is greatest number")
+    else:
+        print(num_3,"is greatest number")
+
+
+#q5
+user_input=input("Give a word:")
+if("name"in user_input):
+    print("Yes")
+else:
+    print("No")
+
+
+#q6
+side_1=int(input("Give a number:"))
+side_2=int(input("Give a number:"))
+side_3=int(input("Give a number:"))
+
+if(side_1+side_2>side_3 and side_2+side_3>side_1 and side_1+side_3>side_2):
+    print("The triangle is possible")
+else:
+    print("The triangle is not possible")
