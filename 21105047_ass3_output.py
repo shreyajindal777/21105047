0ca696d822b7744218859a96ddcecc9d8221f5c8
shreyajindal777 <97687104+shreyajindal777@users.noreply.github.com>
2022-02-13 01:54:49 +0530

    Add files via upload

---
+++ 21105047_ass3_output.py
@@ -0,0 +1,106 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+= RESTART: C:\Users\SHREYA JINDAL\AppData\Local\Programs\Python\Python310\21105047_ass3 input.py
+The solution of Question 1 is:
+
+Enter the string: shreya jindal convinced shreya gupta that they have the same name that is shreya 
+The occurence(s) of:
+	"shreya" is/are 3
+	"jindal" is/are 1
+	"convinced" is/are 1
+	"gupta" is/are 1
+	"that" is/are 2
+	"they" is/are 1
+	"have" is/are 1
+	"the" is/are 1
+	"same" is/are 1
+	"name" is/are 1
+	"is" is/are 1
+
+The solution of Question 2 is:
+
+Day - 29
+Month - 2
+Year - 2023
+The given month has only 28 days
+Please enter a valid date
+Day - 29
+Month - 2
+Year - 2004
+Next Date is: 1/3/2004
+
+The solution of Question 3 is:
+
+Enter the list: [1,2,3,4,5,6,7,8]
+Output: [(1, 1), (2, 4), (3, 9), (4, 16), (5, 25), (6, 36), (7, 49), (8, 64)]
+
+The solution of Question 4 is:
+
+Enter the grade point of the student: 9.5
+Your Grade is 'A' and Excellent Performance
+
+The solution of Question 5 is:
+
+ABCDEFGHIJK
+ ABCDEFGHI
+  ABCDEFG
+   ABCDE
+    ABC
+     A
+
+The solution of Question 6 is:
+
+Enter the name of the student: shreya jindal
+Enter the SID of shreya jindal: 21105047
+
+You have entered 1 value(s) till now
+Do you want to enter more data? y
+Enter the name of the student: arpit singhal
+Enter the SID of arpit singhal: 21105018
+
+You have entered 2 value(s) till now
+Do you want to enter more data? y
+Enter the name of the student: riddhima
+Enter the SID of riddhima: 21105011
+
+You have entered 3 value(s) till now
+Do you want to enter more data? n
+
+a. Student Details:
+The SID of shreya jindal is 21105047
+The SID of arpit singhal is 21105018
+The SID of riddhima is 21105011
+
+b. Student Details (sorted with respect to names):
+The SID of arpit singhal is 21105018
+The SID of riddhima is 21105011
+The SID of shreya jindal is 21105047
+
+c. Student Details (sorted with respect to SIDs):
+The SID of riddhima is 21105011
+The SID of arpit singhal is 21105018
+The SID of shreya jindal is 21105047
+
+d. Enter the SID of the student: 123
+The SID you entered isn't entered
+Please enter a valid SID to be searched
+List of valid SIDs: [21105047, 21105018, 21105011]
+
+Enter the SID of the student: 21105047
+The name of student whose SID is 21105047 is shreya jindal
+
+The solution of Question 7 is:
+
+Enter the no. of terms of the Fibonacci sequence: 9
+The Fibonacci sequence is as follows:
+0 1 1 2 3 5 8 13 21 
+The average of the terms of Fibonacci sequence upto 9 terms is 6.00
+
+The solution of Question 8 is:
+
+a. Set of all elements that are in Set1 and Set2 but not both: {1, 3, 5, 6, 8}
+b. Set of all elements that are in only one of the three sets Set1, Set2 and Set3: {17, 3, 6, 8, 9, 13}
+c. Set of elements that are in exactly two of the sets Set1, Set2 and Set3: {1, 2, 4, 5}
+d. Set of all integers in the range 1 to 10 that are not in Set1: {6, 7, 8, 9, 10}
+e. Set of all integers in the range 1 to 10 that are not in Set1, Set2 and Set3: {10, 7}
