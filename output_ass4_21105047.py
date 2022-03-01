8969ababd66f58b2dda2e96b8c246a7b1f1c5431
shreyajindal777 <97687104+shreyajindal777@users.noreply.github.com>
2022-03-01 22:27:42 +0530

    Add files via upload

---
+++ output_ass4_21105047.py
@@ -0,0 +1,76 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+= RESTART: C:/Users/SHREYA JINDAL/AppData/Local/Programs/Python/Python310/ass_4_input_21105047.py
+Move disk 1 from source A to destination B.
+Move disk 2 from source A to destination C.
+Move disk 1 from source B to destination C.
+Move disk 3 from source A to destination B.
+Move disk 1 from source C to destination A.
+Move disk 2 from source C to destination B.
+Move disk 1 from source A to destination B.
+Enter the size of pascals triangle 5
+USING FOR LOOP
+      1 
+     1 1 
+    1 2 1 
+   1 3 3 1 
+  1 4 6 4 1 
+
+
+
+USING WHILE LOOP
+      1 
+     1 1 
+    1 2 1 
+   1 3 3 1 
+  1 4 6 4 1 
+
+
+
+USING RECURSSIONS
+        1   
+      1   1   
+    1   2   1   
+  1   3   3   1   
+1   4   6   4   1   
+
+
+Enter first integer value(dividend): 456
+Enter second integer value(divisor): 34
+
+Quotient: 13 
+Remainder: 14
+
+a. Check whether the function (divmod()) is callable or not:
+True, which means it is callable
+
+b. Check whether all the values are zero or not:
+All values in result(i.e. quotient and remainder) are non-zero.
+
+c. Add (4,5,6) to the result and filter out values greater than 4:
+Values greater than 4(in list format) are: [5, 6, 13, 14]
+
+d. Convert the above result into a set datatype:
+The output of previous part in set datatype would be: {13, 5, 6, 14}
+
+e. Make the set immutable:
+The immutable set would be: frozenset({13, 5, 6, 14})
+
+f. Evaluate the maximum value from the set and find out its hash value:
+The maximum value from the set is: 14
+The hash value of 14(considering it to be integer) is 14 and its hash value is -7452790390366548081(if we consider 14 as a string).
+Enter name of student: shreya jindal
+Enter SID of shreya jindal: 21105047
+Object Created
+
+The name of the student is shreya jindal and his/her roll number is 21105047
+
+Object destroyed
+a. The updated salary of Mehak is 70000 
+b. Record of Viren deleted
+
+Enter the word: pot
+Enter a new meaningful word using the exact same letters to test your friendship: top
+Does the word makes sense?(y/Y or n/N ) y
+The friends pass the friendship test
